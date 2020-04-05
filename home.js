7378e55109afd70b9394c0a4fade53f3df6cb407
ducphuc-dev <55575330+ducphuc-dev@users.noreply.github.com>
2020-04-05 10:17:31 +0700

    Update home.js

--- home.js
+++ home.js
@@ -1,22 +1,46 @@
 var ex = {
   "name": "Ums - Husc Analyst",
-  "version": "1.2.0",
+  "version": "1.3.0",
   "description": "Tiện ích này giúp bạn quản lý, thống kê điểm của bạn, dành cho các bạn theo học tại Đại học Khoa học Huế",
   "author": "ducphuc-dev",
-  "total_current_user": 110,
-  "rank": 5,
+  "updated_at": "15-04-2020",
+  "total_current_user": 112,
+  "highest_user": 120,
+  "rate_avg": 5,
   "total_reviewer": 17,
   "comment": {
     "count": 14,
     "data": [
       {
-        "user": "Phuc Ngo",
-        "content": "Good"
+        "user": "Nhung Tôn",
+        "avatar": "https://ducphuc-dev.github.io/img/nhung_ton.jpeg",
+        "content": "Chất phết, đỡ phải ngồi tính cần thi mấy điểm",
+        "created_at": "13-02-2020"
       },
       {
-        "user": "Duc Nghia",
-        "content": "Tuyet voi"
-      }
+        "user": "Nghĩa Nguyễn Đức",
+        "avatar": "",
+        "content": "Khá tiện ích với những người lười như mình!",
+        "created_at": "30-01-2020"
+      },
+      {
+        "user": "Tran Van Quang Huy",
+        "avatar": "https://ducphuc-dev.github.io/img/quang_huy.jpeg",
+        "content": "Cung cấp nhiều thông tin hữu ích, perfect <3",
+        "created_at": "12-02-2020"
+      },
+      {
+        "user": "Quốc Khánh Hoàng",
+        "avatar": "https://ducphuc-dev.github.io/img/quoc_khanh.jpeg",
+        "content": "a useful extension...thanks",
+        "created_at": "12-02-2020"
+      },
+      {
+        "user": "Chi Ngô Thị Kim",
+        "avatar": "https://ducphuc-dev.github.io/img/kim_chi.jpeg",
+        "content": "Good",
+        "created_at": "03-03-2020"
+      },
     ] 
   }
 }
