d64274aea5a41d253ebf8a2108766044058865c9
ducphuc-dev <55575330+ducphuc-dev@users.noreply.github.com>
2020-04-23 10:18:48 +0700

    Add files via upload

---
+++ teachers.js
@@ -0,0 +1,110 @@
+var teachers = {
+    "department": [
+        {
+            "name": "CNTT",
+            "teacher_info": [
+                {
+                    name: "Nguyễn Đăng Bình",
+                    degree: "GV-TS",
+                    phone: "0912468119",
+                    email: "nguyendangbinh@gmail.com",
+                    subject: "Khoa học Máy tính",
+                    avatar: "http://it.husc.edu.vn/Media/Image/Avatar/130760310744046819.jpg"
+                },
+                {
+                    name: "Lê Mỹ Cảnh",
+                    degree: "GV-ThS",
+                    phone: "",
+                    email: "lemycanh@gmail.com",
+                    subject: "Công nghệ Phần mềm",
+                    avatar: "http://it.husc.edu.vn/Media/Image/Avatar/129856964415676537.jpg"
+                },
+                {
+                    name: "Trần Thị Phương Chi",
+                    degree: "GV-ThS",
+                    phone: "0903555581",
+                    email: "phuongchi0910@gmail.com",
+                    subject: "Hệ thống Thông tin",
+                    avatar: "http://it.husc.edu.vn/Media/Image/Avatar/130737078221917939.jpg"
+                },
+                {
+                    name: "Lê Quang Chiến",
+                    degree: "GV-TS",
+                    phone: "",
+                    email: "quangchien1512@yahoo.com",
+                    subject: "Hệ thống Thông tin",
+                    avatar: "http://it.husc.edu.vn/Media/Image/Avatar/130759992603303612.jpg"
+                },
+                {
+                    name: "Đặng Thanh Chương",
+                    degree: "GV-TS",
+                    phone: "",
+                    email: "dtchuong@gmail.com",
+                    subject: "Mạng và Truyền thông",
+                    avatar: "http://it.husc.edu.vn/Media/Image/Avatar/130738018856479158.jpg"
+                },
+                {
+                    name: "Nguyễn Dũng",
+                    degree: "GV-ThS",
+                    phone: "0905198887",
+                    email: "nguyendung@hueuni.edu.vn",
+                    subject: "Công nghệ Phần mềm",
+                    avatar: "http://it.husc.edu.vn/Media/Image/Avatar/130419364564108460.jpg"
+                }
+            ]
+        },
+        {
+            "name": "Sinh học",
+            "teacher_info": [
+                {
+                    name: "Nguyễn Minh Trí",
+                    degree: "TS",
+                    phone: "0914031085",
+                    email: "trihatrangthi@gmail.com",
+                    subject: "",
+                    avatar: "http://huebio.husc.edu.vn/vi/img/minhtri.png"
+                },
+                {
+                    name: "Nguyễn Quang Đức Tiến",
+                    degree: "TS",
+                    phone: "0905282824",
+                    email: "nqductien@hueuni.edu.vn",
+                    subject: "",
+                    avatar: "http://huebio.husc.edu.vn/vi/img/Duc%20tien.jpg"
+                },
+                {
+                    name: "Nguyễn Hoàng Lộc",
+                    degree: "GS.TS",
+                    phone: "0977110696",
+                    email: "nhlocs@gmail.com",
+                    subject: "Công nghệ sinh học",
+                    avatar: "http://huebio.husc.edu.vn/vi/img/unnamed-10.png"
+                },
+                {
+                    name: "Hoàng Thị Kim Hồng",
+                    degree: "PGS.TS",
+                    phone: "0978939467",
+                    email: "hkhong@hueuni.edu.vn",
+                    subject: "SHƯD",
+                    avatar: "http://huebio.husc.edu.vn/vi/img/Kim%20hong.png"
+                },
+                {
+                    name: "Lương Quang Đốc",
+                    degree: "TS",
+                    phone: "0914202042",
+                    email: "luongquangdoc@gmail.com",
+                    subject: "TNSV & MT",
+                    avatar: "http://huebio.husc.edu.vn/vi/img/Doc.png"
+                },
+                {
+                    name: "Phạm Thị Ngọc Lan",
+                    degree: "Giảng viên - Thạc sỹ",
+                    phone: "0905237505",
+                    email: "ngoclanz@yahoo.com",
+                    subject: "",
+                    avatar: "http://huebio.husc.edu.vn/vi/img/Pham%20thi%20ngoc%20lan.png"
+                }
+            ]
+        }
+    ]
+}
