8bcfe89759aa4c781b3371141a26ecf239581167
ducphuc-dev <55575330+ducphuc-dev@users.noreply.github.com>
2020-05-10 21:52:10 +0700

    Update home.js

--- home.js
+++ home.js
@@ -4,7 +4,7 @@ const ex = {
   "description": "Tiện ích này giúp bạn quản lý, thống kê điểm của bạn, dành cho các bạn theo học tại Đại học Khoa học Huế",
   "author": "ducphuc-dev",
   "updated_at": "22-04-2020",
-  "total_current_user": 128,
+  "total_current_user": 123,
   "highest_user": 130,
   "rate_avg": 5,
   "total_reviewer": 17,
