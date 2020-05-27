34564a3519d0bfcef50c384fcf2d0a27721194cb
ducphuc-dev <55575330+ducphuc-dev@users.noreply.github.com>
2020-05-27 19:12:10 +0700

    Update home.js

--- home.js
+++ home.js
@@ -1,9 +1,9 @@
 const ex = {
   "name": "Ums - Husc Analyst",
-  "version": "1.2.2",
+  "version": "1.2.3",
   "description": "Tiện ích này giúp bạn quản lý, thống kê điểm của bạn, dành cho các bạn theo học tại Đại học Khoa học Huế",
   "author": "ducphuc-dev",
-  "updated_at": "22-04-2020",
+  "updated_at": "27-05-2020",
   "total_current_user": 130,
   "highest_user": 130,
   "rate_avg": 5,
@@ -102,6 +102,13 @@ const ex = {
     },
     {
       "stt": 6,
+      "version": "v1.2.3",
+      "content": "Sửa lỗi truy cập bằng phương thức HTTPS mới của trang tín chỉ",
+      "status": "Xuất bản",
+      "launch_date": "27-05-2020"
+    },
+    {
+      "stt": 7,
       "version": "v1.3.1",
       "content": "Tính năng mới: Tạo ghi chú hẹn giờ, Xem thông tin giảng viên. Cho phép đi đến trang tin nhắn của trang tín chỉ khi nhấn vào thông báo",
       "status": "Đang xây dựng",
